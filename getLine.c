08565833192797960f3f7f9b7e830cd11e09a991
koffsinmax <ezekielappiah8@gmail.com>
2024-01-16 16:34:57 -0800

    update

--- getLine.c
+++ getLine.c
@@ -1,13 +1,14 @@
-#include "shell.h"
+#include  "shell.h"
 
 /**
- * input_buf - buffers chained commands
- * @info: parameter struct
- * @buf: address of buffer
- * @len: address of len var
+ * input_buf - buffer chaned commands
+ * @info: parameter  struct
+ * @buf:  address of buffer
+ * @len:  address of len var
  *
- * Return: bytes read
+ * Return:  bytes read
  */
+
 ssize_t input_buf(info_t *info, char **buf, size_t *len)
 {
 	ssize_t r = 0;
